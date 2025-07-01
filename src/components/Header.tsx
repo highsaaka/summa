8dc3840c5c0fce687aa8c331462badbaa22ce2b4
highsaaka <94737083+highsaaka@users.noreply.github.com>
2025-07-01 09:47:43 +0000

    initial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import { Link } from '@tanstack/react-router'
+
+export default function Header() {
+  return (
+    <header className="p-2 flex gap-2 bg-white text-black justify-between">
+      <Button>
+        hello 
+      </Button>
+    </header>
+  )
+}
